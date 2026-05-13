474e18697d3f5b52677d2d3716a49d735fc9732e
Jose Valerio <contact@josevalerio.com>
2026-05-13 03:16:53 -0400

    saving

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,3 @@
-#![warn(clippy::all, clippy::pedantic, clippy::nursery)]
-mod colors;
-mod constants;
-mod globals;
-mod logger;
-mod macros;
-mod utils;
-
-pub use colors::RGB;
-pub use globals::get_global_logger;
-pub use logger::{LogLevel, Logger, LoggerOptions};
+pub fn hi() {
+    println!("hi")
+}
--- src/utils.rs
+++
@@ -1,5 +0,0 @@
-mod flush_batch;
-mod write_log_line;
-
-pub use flush_batch::flush_batch;
-pub use write_log_line::{FormatState, RESERVED_FIELD_NAMES, write_log_line};
